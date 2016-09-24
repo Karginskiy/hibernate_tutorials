f8a3bfad9033aa842a469672a9297d15510c05b3
Karginskiy <hei@spark-mail.ru>
2016-09-24 20:29:17 +0300

    Artist class is maded of Bean convention

--- src/main/src/Artist.java
+++ src/main/src/Artist.java
@@ -1,2 +1,58 @@
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Artist {
+
+    @Id
+    private int id;
+
+    private String name;
+    private String genre;
+
+    public Artist(int id, String name, String genre) {
+        this.id = id;
+        this.name = name;
+        this.genre = genre;
+    }
+
+    public Artist() {
+
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+
+        this.genre = genre;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Artist{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", genre='" + genre + '\'' +
+                '}';
+    }
 }
