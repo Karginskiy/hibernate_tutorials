d634772771eac10bc19fcafc06320cb56d606f7d
Karginskiy <hei@spark-mail.ru>
2016-09-25 13:52:05 +0300

    Created class User

---
+++ src/main/src/User.java
@@ -0,0 +1,63 @@
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "user", schema = "public")
+public class User {
+
+    private Long userId;
+    private String name;
+    private String login;
+    private String password;
+    private Integer role;
+    private List<Task> tasks;
+
+    @Id
+    @SequenceGenerator(name = "user_seq", sequenceName = "user_user_id_seq", allocationSize = 0)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    @Column(name = "user_id")
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user", cascade = CascadeType.ALL)
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "login")
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    @Column(name = "password")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
