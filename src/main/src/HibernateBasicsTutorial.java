b9e6b0aa27f05be12866aef64b774a2d56a17c73
Karginskiy <hei@spark-mail.ru>
2016-09-25 13:03:20 +0300

    Added comments to HibernateBasicsTutorial

--- src/main/src/HibernateBasicsTutorial.java
+++ src/main/src/HibernateBasicsTutorial.java
@@ -7,27 +7,40 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 
 import java.util.List;
 
+/**
+ * This class just checking the work of service.
+ */
+
 public class HibernateBasicsTutorial {
 
     public static void main(String[] args) {
 
+        // Configuration instance from the hibernate.cfg.xml
         Configuration configuration = new Configuration().configure();
+        // Create registry for the current settings
+        // From this registry we are getting connections to your tables
         StandardServiceRegistry serviceRegistry =
                 new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+        // Create session factory for the current config.
         SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        // Getting session from the factory
         Session session = sessionFactory.openSession();
+        // Creating ArtistService for the taken session.
         ArtistService service = new ArtistService(session);
 
+        // Testing the createArtist function.
         System.out.println("--- Create and persist artist ---");
         Transaction transaction = session.beginTransaction();
         Artist artist = service.createArtist(1, "John Lennon", "Rock");
         transaction.commit();
         System.out.println(String.format("Persisted: %s\n", artist));
 
+        // Testing the findArtist function.
         System.out.println("--- Find artist ---");
         artist = service.findArtist(1);
         System.out.println(String.format("Found: %s\n", artist));
 
+        // Testing the findAllArtist function.
         System.out.println("--- Find all artists ---");
         List<Artist> artists = service.findAllArtists();
         for (Artist currentArtist: artists)
@@ -35,12 +48,14 @@ public class HibernateBasicsTutorial {
             System.out.println(String.format("Found: %s\n", currentArtist));
         }
 
+        // Testing the changeArtistGenre function.
         System.out.println("--- Update artist ---");
         transaction = session.beginTransaction();
         artist = service.changeArtistGenre(1, "Indie Rock");
         transaction.commit();
         System.out.println(String.format("Updated: %s\n", artist));
 
+        // Testing the removeArtist function.
         System.out.println("--- Remove artist ---");
         transaction = session.beginTransaction();
         service.removeArtist(1);
@@ -48,6 +63,7 @@ public class HibernateBasicsTutorial {
         artist= service.findArtist(1);
         System.out.println(String.format("Found: %s\n", artist));
 
+        // Closing session and factory
         session.close();
         sessionFactory.close();
 
