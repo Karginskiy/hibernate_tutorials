f41c3b3ff17b362fa1a7e9df73974aee2f43aa70
Karginskiy <hei@spark-mail.ru>
2016-09-25 11:49:42 +0300

    Added tests for create, get and getAll.

--- src/main/src/HibernateBasicsTutorial.java
+++ src/main/src/HibernateBasicsTutorial.java
@@ -1,8 +1,11 @@
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.boot.registry.StandardServiceRegistry;
+
+import java.util.List;
 
 public class HibernateBasicsTutorial {
 
@@ -15,6 +18,22 @@ public class HibernateBasicsTutorial {
         Session session = sessionFactory.openSession();
         ArtistService service = new ArtistService(session);
 
+        System.out.println("--- Create and persist artist ---");
+        Transaction transaction = session.beginTransaction();
+        Artist artist = service.createArtist(1, "John Lennon", "Rock");
+        transaction.commit();
+        System.out.println(String.format("Persisted: %s\n", artist));
+
+        System.out.println("--- Find artist ---");
+        artist = service.findArtist(1);
+        System.out.println(String.format("Found: %s\n", artist));
+
+        System.out.println("--- Find all artists ---");
+        List<Artist> artists = service.findAllArtists();
+        for (Artist currentArtist: artists)
+        {
+            System.out.println(String.format("Found: %s\n", currentArtist));
+        }
 
     }
 
