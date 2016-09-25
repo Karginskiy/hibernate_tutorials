ca0c6ec081d2e9fa7bce67ac7842d8c67265e77a
Karginskiy <hei@spark-mail.ru>
2016-09-25 12:09:06 +0300

    Added comment at the Artist

--- src/main/src/Artist.java
+++ src/main/src/Artist.java
@@ -4,12 +4,23 @@ import javax.persistence.Id;
 @Entity
 public class Artist {
 
+    /**
+     * This is a main class for using by SQL Server. Bean-typed class.
+     * @Id id - annotation for the hibernate's primary key - INTEGER;
+     * String name - is for the db.name - VARCHAR(30);
+     * String genre - is for the db.genre - VARCHAR(20).
+     */
+
+
     @Id
     private int id;
 
     private String name;
     private String genre;
 
+    /**
+     * Constructor for every query
+     */
     public Artist(int id, String name, String genre) {
         this.id = id;
         this.name = name;
