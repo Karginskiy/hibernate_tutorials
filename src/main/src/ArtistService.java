8c91a2212ddb76105677b45d6a6b1e8e57ce719d
Karginskiy <hei@spark-mail.ru>
2016-09-25 12:29:49 +0300

    Added comments for the ArtistService

--- src/main/src/ArtistService.java
+++ src/main/src/ArtistService.java
@@ -5,12 +5,31 @@ import java.util.List;
 
 public class ArtistService {
 
+    /**
+     * This class is a service with a CRUD-queries.
+     * session - is instance of current transaction.
+     * @see ArtistService#ArtistService(Session)
+     */
+
     private Session session;
 
+    /**
+     * Constructor for the service's instance. Had a current transaction as an argument.
+     * @param session - Session-typed. This param is the logical transaction.
+     * @see ArtistService#createArtist(int, String, String)
+     */
     public ArtistService(Session session) {
         this.session = session;
     }
 
+    /**
+     * This method is just a insert-query, just like <b>INSERT INTO table_name (col1, col2, col3) VALUES (id, name, genre)</b>
+     * @param id - is for db's Primary Key
+     * @param name - is for db's name VARCHAR
+     * @param genre - is for db's genre VARCHAR
+     * @return Artist-instance that was created.
+     * @see ArtistService#removeArtist(int)
+     */
     public Artist createArtist(int id, String name, String genre)
     {
         Artist artist = new Artist(id, name, genre);
@@ -18,6 +37,11 @@ public class ArtistService {
         return artist;
     }
 
+    /**
+     * This method is removing Artist for DB, just as you queries <b>DELETE FROM table_name WHERE table_name.id = id</b>.
+     * @param id - is for db's Primary Key
+     * @see ArtistService#changeArtistGenre(int, String)
+     */
     public void removeArtist(int id)
     {
         Artist artist = (Artist) session.get(Artist.class, id);
@@ -28,6 +52,14 @@ public class ArtistService {
         }
     }
 
+    /**
+     * This method is for changing Artist's genre in db by id. Just like <b>UPDATE table_name SET table_name.genre=genre
+     *  WHERE table_name.id = id</b>.
+     * @param id - is for queried Artist pk.
+     * @param genre - new genre for this Artist.
+     * @return - Artist instance that was changed.
+     * @see ArtistService#findArtist(int)
+     */
     public Artist changeArtistGenre(int id, String genre)
     {
         Artist artist = (Artist) session.get(Artist.class, id);
@@ -40,11 +72,22 @@ public class ArtistService {
         return artist;
     }
 
+    /**
+     * This method is returning instance by pk. SELECT a FROM table_name WHERE table_name.id = id.
+     * @param id - pk
+     * @return - Artist instance that you've taken.
+     * @see ArtistService#findAllArtists()
+     */
     public Artist findArtist(int id)
     {
         return (Artist) session.get(Artist.class, id);
     }
 
+    /**
+     * This method is for getting all the instances from db.
+     * @return - List<Artist>
+     * @see ArtistService
+     */
     public List<Artist> findAllArtists() {
         Query query = session.createQuery("SELECT a FROM Artist a");
         return query.list();
