4037b22f0863594a4085550ff3fd29e209428ba4
Karginskiy <hei@spark-mail.ru>
2016-09-25 11:31:10 +0300

    Added session openings for the current configuration.

--- src/main/src/HibernateBasicsTutorial.java
+++ src/main/src/HibernateBasicsTutorial.java
@@ -1,2 +1,21 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+
 public class HibernateBasicsTutorial {
+
+    public static void main(String[] args) {
+
+        Configuration configuration = new Configuration().configure();
+        StandardServiceRegistry serviceRegistry =
+                new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+        SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        Session session = sessionFactory.openSession();
+        ArtistService service = new ArtistService(session);
+
+
+    }
+
 }
